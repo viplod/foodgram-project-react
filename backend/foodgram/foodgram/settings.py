03dc3dd1dc96c0cc363d7e533a7d62efcdda7df9
Viplod <viplod@yandex.ru>
2022-10-06 16:11:53 +0500

    изменения settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -118,7 +118,6 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
